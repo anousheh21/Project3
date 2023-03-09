d65bc86b21607f87bd954145a48e97f40a4a0a07
anousheh21 <anousheh21@gmail.com>
2023-03-09 06:15:13 +0000

    Added base code

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+// console.log(process.env.JWT_SECRET)
+
+// Connects to MongoDB database
+mongoose.connect(process.env.CONNECT_DB)
+
+// Middleware
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use('/', express.static('./public'))
+
+// Requests
+
+
+
+// Connect to Port
+const port = 5000
+app.listen(port, () => {
+    console.log(`Server Live on Port ${port}`)
+})
